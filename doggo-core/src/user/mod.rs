883089873935bf655e42bec2f6d344f586efea08
Peter Farr <Peter@PrismaPhonic.com>
2019-11-11 21:50:43 -0800

    added collection abstraction for users collection

--- doggo-core/src/user/mod.rs
+++ doggo-core/src/user/mod.rs
@@ -1,22 +1,33 @@
-use domain_patterns::models::ValueObject;
-use crate::Result;
+use domain_patterns::models::{ValueObject, Entity, AggregateRoot};
+use crate::{Result, Error};
 use crate::value_objects::{Username, Password, Email};
 use std::convert::TryFrom;
+use ulid::Ulid;
 
 /// User represents a user in our system.  The password value object always stores an encrypted string.
+#[derive(Entity, Clone)]
 pub struct User {
+    id: Ulid,
     email: Email,
-    username: Username,
     password: Password,
 }
 
 impl User {
-    pub fn new(email: String, username: String, password: String) -> Result<User> {
+    pub fn new(email: String, password: String) -> Result<User> {
         Ok(User {
+            id: Ulid::new(),
             email: Email::try_from(email)?,
-            username: Username::try_from(username)?,
             password: Password::try_from(password)?,
         })
+    }
+
+    /// This method allows you to construct a user bypassing validation from raw values.
+    pub fn new_raw(id: String, email: String, password: String) -> User {
+        User {
+            id: Ulid::from_string(&id).unwrap(),
+            email: Email { value: email },
+            password: Password { value: password },
+        }
     }
 
     pub fn validate_password(&self, password: String) -> bool {
