35f2bd69a491640b06cb15b0d583361f97726063
Peter Farr <Peter@PrismaPhonic.com>
2019-11-15 14:35:18 -0800

    Changed queries since in test example we don't use replicas

--- doggo-infra/src/repositories/pupper_repository.rs
+++ doggo-infra/src/repositories/pupper_repository.rs
@@ -1,9 +1,9 @@
-use mysql;
-use doggo_core::dtos::Pupper;
-use super::CLIENT_POOL;
 use super::Conn;
-use indexmap::IndexMap;
+use super::CLIENT_POOL;
 use doggo_core::collection_abstractions::PupperRepository;
+use doggo_core::dtos::Pupper;
+use indexmap::IndexMap;
+use mysql;
 
 const RETRY_COUNT: i32 = 5;
 
@@ -25,8 +25,9 @@ impl VitessPupperRepository {
     fn puppers_rating(&mut self, id: u64) -> Result<Option<f64>, mysql::Error> {
         match self.conn.prep_exec(
             r"SELECT COALESCE(SUM(r.rating)/COUNT(r.rating),0.0)
-            FROM puppers@replica.ratings as r
-            WHERE r.pupper_id=?", (id,)
+            FROM ratings as r
+            WHERE r.pupper_id=?",
+            (id,),
         ) {
             Ok(mut qr) => {
                 if let Some(row_result) = qr.next() {
@@ -41,19 +42,15 @@ impl VitessPupperRepository {
 
     fn pup_with_rating(
         &mut self,
-        pup_data: (u64, String, String)
+        pup_data: (u64, String, String),
     ) -> Result<Option<Pupper>, mysql::Error> {
         let maybe_rating = self.puppers_rating(pup_data.0)?;
-        return Ok(
-            Some(
-                Pupper {
-                    id: pup_data.0,
-                    name: pup_data.1,
-                    image: pup_data.2,
-                    rating: maybe_rating,
-                }
-            )
-        );
+        return Ok(Some(Pupper {
+            id: pup_data.0,
+            name: pup_data.1,
+            image: pup_data.2,
+            rating: maybe_rating,
+        }));
     }
 
     fn puppers_from_rating_list(
@@ -62,30 +59,46 @@ impl VitessPupperRepository {
     ) -> Result<Vec<Pupper>, mysql::Error> {
         // Cloning primitives is cheap, so doing so is virtually cost free for such a small list.
         // Using IndexMap because it maintains insertion order.
-        let mut pupper_map: IndexMap<u64, Pupper> = list.iter().map(|(i, r)| {
-            (i.clone(), Pupper {
-                id: i.clone(),
-                name: "".to_string(),
-                image: "".to_string(),
-                rating: Some(*r)
+        let mut pupper_map: IndexMap<u64, Pupper> = list
+            .iter()
+            .map(|(i, r)| {
+                (
+                    i.clone(),
+                    Pupper {
+                        id: i.clone(),
+                        name: "".to_string(),
+                        image: "".to_string(),
+                        rating: Some(*r),
+                    },
+                )
             })
-        }).collect();
+            .collect();
 
-        let ids_str: String = list.iter().map(|(i, _)| i.to_string()).collect::<Vec<String>>().join("','");
+        let ids_str: String = list
+            .iter()
+            .map(|(i, _)| i.to_string())
+            .collect::<Vec<String>>()
+            .join("','");
 
-        self.conn.query(
-            format!(r"SELECT p.id as id, p.name as name, p.image as image
+        self.conn
+            .query(format!(
+                r"SELECT p.id as id, p.name as name, p.image as image
             FROM puppers AS p
             WHERE id in
-            ('{}')", ids_str)
-        ).map(|result| {
-            result.for_each(|row_result| {
-                if let Ok(r) = row_result {
-                    let (id, name, image) = mysql::from_row(r);
-                    pupper_map.entry(id).and_modify(|p| { p.name = name; p.image = image});
-                }
-            })
-        })?;
+            ('{}')",
+                ids_str
+            ))
+            .map(|result| {
+                result.for_each(|row_result| {
+                    if let Ok(r) = row_result {
+                        let (id, name, image) = mysql::from_row(r);
+                        pupper_map.entry(id).and_modify(|p| {
+                            p.name = name;
+                            p.image = image
+                        });
+                    }
+                })
+            })?;
 
         let puppers = pupper_map.into_iter().map(|(_, p)| p).collect();
 
@@ -97,26 +110,26 @@ impl PupperRepository for VitessPupperRepository {
     type Error = mysql::Error;
 
     fn get(&mut self, pupper_id: u64) -> Result<Option<Pupper>, mysql::Error> {
-        let r: Option<(u64, String, String)> =
-            match self.conn.prep_exec(
-                r"SELECT p.id, p.name, p.image
+        let r: Option<(u64, String, String)> = match self.conn.prep_exec(
+            r"SELECT p.id, p.name, p.image
                 FROM puppers AS p
-                WHERE p.id = ?", (pupper_id,)
-            ) {
-                Ok(mut qr) => {
-                    if let Some(row_result) = qr.next() {
-                        let row = row_result?;
-                        Some(mysql::from_row::<(u64, String, String)>(row))
-                    } else {
-                        None
-                    }
-                },
-
-                // Underlying MySQL error type unrelated to existence of puppers in db.
-                Err(e) => {
-                    return Err(e);
+                WHERE p.id = ?",
+            (pupper_id,),
+        ) {
+            Ok(mut qr) => {
+                if let Some(row_result) = qr.next() {
+                    let row = row_result?;
+                    Some(mysql::from_row::<(u64, String, String)>(row))
+                } else {
+                    None
                 }
-            };
+            }
+
+            // Underlying MySQL error type unrelated to existence of puppers in db.
+            Err(e) => {
+                return Err(e);
+            }
+        };
 
         if let Some(pup) = r {
             return self.pup_with_rating(pup);
@@ -127,27 +140,26 @@ impl PupperRepository for VitessPupperRepository {
     }
 
     fn get_random(&mut self) -> Result<Option<Pupper>, mysql::Error> {
-        let r: Option<(u64, String, String)> =
-            match self.conn.query(
-                r"SELECT p.id, p.name, p.image
-                FROM puppers@replica.puppers AS p
+        let r: Option<(u64, String, String)> = match self.conn.query(
+            r"SELECT p.id, p.name, p.image
+                FROM puppers AS p
                 ORDER BY RAND()
-                LIMIT 1"
-            ) {
-                Ok(mut qr) => {
-                    if let Some(row_result) = qr.next() {
-                        let row = row_result?;
-                        Some(mysql::from_row::<(u64, String, String)>(row))
-                    } else {
-                        None
-                    }
-                },
-
-                // Underlying MySQL error type unrelated to existence of puppers in db.
-                Err(e) => {
-                    return Err(e);
+                LIMIT 1",
+        ) {
+            Ok(mut qr) => {
+                if let Some(row_result) = qr.next() {
+                    let row = row_result?;
+                    Some(mysql::from_row::<(u64, String, String)>(row))
+                } else {
+                    None
                 }
-            };
+            }
+
+            // Underlying MySQL error type unrelated to existence of puppers in db.
+            Err(e) => {
+                return Err(e);
+            }
+        };
 
         if let Some(pup) = r {
             return self.pup_with_rating(pup);
@@ -181,23 +193,24 @@ impl PupperRepository for VitessPupperRepository {
 
 // PupperRepositoryRetryWrapper is a retry wrapper that is agnostic to underlying storage.
 pub struct PupperRepositoryRetryWrapper<T>
-    where T: PupperRepository
+where
+    T: PupperRepository,
 {
-    repo: T
+    repo: T,
 }
 
 impl<T> PupperRepositoryRetryWrapper<T>
-    where T: PupperRepository
+where
+    T: PupperRepository,
 {
     pub fn new(repo: T) -> PupperRepositoryRetryWrapper<T> {
-        PupperRepositoryRetryWrapper {
-            repo,
-        }
+        PupperRepositoryRetryWrapper { repo }
     }
 }
 
 impl<T> PupperRepository for PupperRepositoryRetryWrapper<T>
-    where T: PupperRepository
+where
+    T: PupperRepository,
 {
     type Error = T::Error;
 
