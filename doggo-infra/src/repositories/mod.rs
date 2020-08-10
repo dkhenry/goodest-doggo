477022a882cee7d8440a2f2f901c4326083ddf8d
Mike Cronce <mc@planetscale.com>
2020-08-10 16:48:51 -0400

    doggo-infra/src/repositories/mod.rs:  Add query wrappers so we can check for errors ourselves (and add error checking to set_url() and get_conn()) and mark the connection as not working if we have errors

--- doggo-infra/src/repositories/mod.rs
+++ doggo-infra/src/repositories/mod.rs
@@ -1,38 +1,108 @@
 use std::sync::Arc;
 use std::sync::Mutex;
 
+use mysql::prelude::FromRow;
+use mysql::prelude::Queryable;
+
 // Convenience type for handlers.
 #[derive(Clone)]
 pub struct ConfigurablePool {
-    inner: Option<mysql::Pool>
+    inner: Option<mysql::Pool>,
+    is_working: bool
 }
 
 #[derive(Clone)]
 pub struct Pool(Arc<Mutex<ConfigurablePool>>);
 
+// TODO:  impl Queryable for Pool
+// TODO:  DRY
 impl Pool {
     fn new(inner: Option<mysql::Pool>) -> Self {
         Self{
             0: Arc::new(Mutex::new(ConfigurablePool{
-                inner: inner
+                inner: inner,
+                is_working: true
             }))
         }
     }
 
     pub fn get_conn(&self) -> Result<mysql::PooledConn, mysql::Error> {
-        let guard = &self.0.lock().unwrap();
+        let guard = &mut self.0.lock().unwrap();
         let inner = guard.inner.as_ref().unwrap();
-        inner.get_conn()
+        match inner.get_conn() {
+            Ok(v) => Ok(v),
+            Err(e) => {
+                guard.is_working = false;
+                Err(e)
+            }
+        }
     }
 
-    pub fn set_url(&self, url: impl AsRef<str>) {
+    pub fn set_url(&self, url: impl AsRef<str>) -> Result<(), mysql::Error> {
         let guard = &mut self.0.lock().unwrap();
-        guard.inner = Some(mysql::Pool::new(&url).unwrap());
+        guard.inner = Some(match mysql::Pool::new(&url) {
+            Ok(v) => {
+                guard.is_working = true;
+                v
+            },
+            Err(e) => {
+                guard.is_working = false;
+                return Err(e)
+            }
+        });
+        Ok(())
     }
 
     pub fn is_configured(&self) -> bool {
         let guard = &self.0.lock().unwrap();
         guard.inner.is_some()
+    }
+
+    pub fn is_working(&self) -> bool {
+        let guard = &self.0.lock().unwrap();
+        guard.inner.is_some() && guard.is_working
+    }
+
+    pub fn query<T: FromRow>(&self, query: impl AsRef<str>) -> mysql::Result<Vec<T>> {
+        let mut connection = self.get_conn()?;
+        match connection.query(query.as_ref()) {
+            Ok(v) => {
+                self.0.lock().unwrap().is_working = true;
+                Ok(v)
+            },
+            Err(e) => {
+                self.0.lock().unwrap().is_working = false;
+                Err(e)
+            }
+        }
+    }
+
+    pub fn query_first<T: FromRow>(&self, query: impl AsRef<str>) -> mysql::Result<Option<T>> {
+        let mut connection = self.get_conn()?;
+        match connection.query_first(query.as_ref()) {
+            Ok(v) => {
+                self.0.lock().unwrap().is_working = true;
+                Ok(v)
+            },
+            Err(e) => {
+                self.0.lock().unwrap().is_working = false;
+                Err(e)
+            }
+        }
+    }
+
+    pub fn query_drop(&self, query: impl AsRef<str>) -> mysql::Result<()> {
+        let mut connection = self.get_conn()?;
+        match connection.query_drop(query.as_ref()) {
+            Ok(v) => {
+                self.0.lock().unwrap().is_working = true;
+                Ok(v)
+            },
+            Err(e) => {
+                self.0.lock().unwrap().is_working = false;
+                Err(e)
+            }
+        }
     }
 }
 
