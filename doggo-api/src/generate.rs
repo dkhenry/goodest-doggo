7ad1eb4972fd0fd1fc5b45b46ccc490763d2587b
Mike Cronce <mc@planetscale.com>
2020-08-12 16:49:21 -0400

    doggo-api/src/generate.rs:  + data query stuff

--- doggo-api/src/generate.rs
+++ doggo-api/src/generate.rs
@@ -1,5 +1,6 @@
 use doggo_core::queries::pupper_query_handler::VitessPupperQueriesHandler;
-use doggo_infra::repositories::{VitessPupperRepository, VitessBallotRepository, PupperRepositoryRetryWrapper, VitessUserRepository};
+use doggo_core::queries::data_query_handler::VitessDataQueryHandler;
+use doggo_infra::repositories::{VitessPupperRepository, VitessBallotRepository, PupperRepositoryRetryWrapper, VitessUserRepository, VitessDataRepository};
 use doggo_core::commands::pupper_commands_handler::VitessPupperCommandHandler;
 use doggo_core::commands::user_commands_handler::VitessUserCommandHandler;
 
@@ -19,3 +20,9 @@ pub fn query_handler() -> VitessPupperQueriesHandler<PupperRepositoryRetryWrappe
     let handler = VitessPupperQueriesHandler::new(wrapped_repo);
     handler
 }
+
+pub fn data_query_handler() -> VitessDataQueryHandler<VitessDataRepository> {
+	let repo = VitessDataRepository::new();
+	VitessDataQueryHandler::new(repo)
+}
+
